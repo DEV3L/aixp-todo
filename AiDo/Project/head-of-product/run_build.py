175f37c6af46bb341ddb28027a5918b20b676116
DEV3L <jus.beall@gmail.com>
2024-06-09 00:17:15 -0400

    Update github actions to include both projects

---
+++ AiDo/Project/head-of-product/run_build.py
@@ -0,0 +1,43 @@
+import sys
+
+sys.path.append("../trello-extract/src")
+
+from loguru import logger
+
+from src.assistants.assistant_service import (
+    ASSISTANT_NAME,
+    AssistantService,
+)
+from src.clients.openai_api import OpenAIClient, build_openai_client
+from src.exporters.blogs.blogs_exporter import BlogsExporter
+from src.exporters.files.file_exporter import FileExporter
+
+
+def export_data():
+    BlogsExporter().export()
+    FileExporter("AiDo Product Definition.txt").export()
+    FileExporter("AiDo Website.txt").export()
+    FileExporter("Persona - Head of Product - Alex Parker.txt").export()
+
+
+def main():
+    logger.info(f"Building {ASSISTANT_NAME}")
+
+    export_data()
+
+    client = OpenAIClient(build_openai_client())
+    service = AssistantService(client)
+
+    logger.info("Removing existing assistant and category files")
+    # service.delete_assistant()
+
+    # assistant_id = service.get_assistant_id()
+
+    # logger.info(f"Assistant ID: {assistant_id}")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logger.info(f"Error: {e}")
--- AiDo/Project/trello-extract/src/orchestrators/orchestration_service.py
+++ AiDo/Project/trello-extract/src/orchestrators/orchestration_service.py
@@ -12,7 +12,7 @@ class OrchestrationService:
     def write_board_markdown_to_file(self, board_name: str, directory: str) -> str:
         markdown_content = self.get_board_markdown(board_name)
         os.makedirs(directory, exist_ok=True)
-        file_path = os.path.join(directory, f"{board_name} Trello.txt")
+        file_path = os.path.join(directory, f"{board_name} Status Trello Board.txt")
         with open(file_path, "w") as file:
             file.write(markdown_content)
         return file_path
